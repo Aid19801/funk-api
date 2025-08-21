9106e6e67d779910c1a79c66067c559129c3644b
Adrian Thompson <adrian.thompson@tradenation.com>
2025-08-21 20:42:21 +0200

    working queries

---
+++ db.py
@@ -0,0 +1,12 @@
+import psycopg2
+from config import SECRET_KEY, DATABASE_URL
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+def get_conn():
+    return psycopg2.connect(DATABASE_URL)
--- util.py
+++ util.py
@@ -1,18 +1,33 @@
 from fastapi import Depends, Request, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 import jwt  # PyJWT
-from config import SECRET_KEY
+from db import SECRET_KEY
+from db import get_conn
 
 auth_scheme = HTTPBearer()
 
 def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(auth_scheme)):
+    print("BEEEEEEE ===========")
+    print("Raw credentials:", credentials.credentials)
     token = credentials.credentials
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
-        email = payload.get("sub")
-        if not email:
+        print("Decoded payload:", payload)
+        user_id = payload.get("sub")
+        print("user_id??", user_id)
+        if not user_id:
             raise HTTPException(status_code=401, detail="Invalid token.")
-        return email
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute("SELECT id, email FROM users WHERE id = %s", (user_id,))
+        user = cur.fetchone()
+        print("found user? ", user)
+        cur.close()
+        conn.close()
+
+        if not user:
+            raise HTTPException(status_code=401, detail="User not found.")
+        return {"id": user[0], "email": user[1]}
     except jwt.ExpiredSignatureError:
         raise HTTPException(status_code=401, detail="Token expired.")
     except jwt.InvalidTokenError:
