703ba265e7061b06628d106c0c4380ed782685be
Adrian Thompson <adrian.thompson@tradenation.com>
2025-12-18 07:01:17 +0000

    fixed imports

--- db.py
+++ db.py
@@ -1,11 +1,11 @@
 import psycopg2
-from config import SECRET_KEY, DATABASE_URL
+from config import DATABASE_URL
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 def get_conn():
